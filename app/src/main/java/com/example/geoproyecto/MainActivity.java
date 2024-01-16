7ad1515980fc7f90c970f3d6eab5d308e55c6c69
rafanules <rafvilcli@alu.edu.gva.es>
2024-01-16 12:57:42 +0100

    primero

---
+++ app/src/main/java/com/example/geoproyecto/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.geoproyecto;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.geoproyecto.ui.home.SharedViewModel;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.geoproyecto.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private ActivityResultLauncher<String[]> locationPermissionRequest;
+    private SharedViewModel sharedViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        sharedViewModel = new ViewModelProvider(this).get(SharedViewModel.class);
+
+        FusedLocationProviderClient mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        sharedViewModel.setFusedLocationClient(mFusedLocationClient);
+
+        sharedViewModel.getCheckPermission().observe(this, s -> checkPermission());
+
+        locationPermissionRequest = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
+            Boolean fineLocationGranted = result.getOrDefault(android.Manifest.permission.ACCESS_FINE_LOCATION, false);
+            Boolean coarseLocationGranted = result.getOrDefault(android.Manifest.permission.ACCESS_COARSE_LOCATION, false);
+            if (fineLocationGranted != null && fineLocationGranted) {
+                sharedViewModel.startTrackingLocation(false);
+            } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                sharedViewModel.startTrackingLocation(false);
+            } else {
+                Toast.makeText(this, "No concedeixen permisos", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    void checkPermission() {
+        Log.d("PERMISSIONS", "Check permisssions");
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            Log.d("PERMISSIONS", "Request permisssions");
+            locationPermissionRequest.launch(new String[]{
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+            });
+        } else {
+            sharedViewModel.startTrackingLocation(false);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/geoproyecto/ui/home/HomeFragment.java
@@ -0,0 +1,94 @@
+package com.example.geoproyecto.ui.home;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.GnssAntennaInfo;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.geoproyecto.databinding.FragmentHomeBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LastLocationRequest;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class HomeFragment extends Fragment {
+
+
+    private FragmentHomeBinding binding;
+
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        SharedViewModel sharedViewModel = new ViewModelProvider(getActivity()).get(SharedViewModel.class);
+
+        SharedViewModel.getCurrentAddress().observe(getViewLifecycleOwner(), address -> {
+            binding.TextLocation.setText(String.format(
+                    "Direcció: %1$s \n Hora: %2$tr",
+                    address, System.currentTimeMillis()));
+        });
+        sharedViewModel.getButtonText().observe(getViewLifecycleOwner(), s -> binding.GetLocation.setText(s));
+        sharedViewModel.getProgressBar().observe(getViewLifecycleOwner(), visible -> {
+            if (visible)
+                binding.loading.setVisibility(ProgressBar.VISIBLE);
+            else
+                binding.loading.setVisibility(ProgressBar.INVISIBLE);
+        });
+
+        binding.GetLocation.setOnClickListener(view -> {
+            Log.d("DEBUG", "Clicked Get Location");
+            sharedViewModel.switchTrackingLocation();
+        });
+
+
+
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
+
+
+
+
+
